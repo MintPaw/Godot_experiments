6b8b417b52c4014cb252b3a235c9d308a211a58e
MintPaw <jerusanders@gmail.com>
2016-07-24 16:03:12 -0600

    Fixed little syntax things

--- player_run_loop.gd
+++ player_run_loop.gd
@@ -24,79 +24,78 @@ func _process(delta):
 	tempElapsed = tempElapsed + delta
 	runElapsed = runElapsed + delta
 	#If the player is running right
-	if( Input.is_action_pressed("run_right")):
-		if(direction == -1):
+	if Input.is_action_pressed("run_right"):
+		if direction == -1:
 			direction = 1
 			self.set_flip_h(false)
 		#runElapsed for smoother running
-		if(runElapsed > 0.05):
+		if runElapsed > 0.05:
 			runElapsed = 0
 			pos.x += speed.x
 			set_pos(pos)
 		self.set_pos(get_pos())
-		if(tempElapsed > 0.1):
-			if(run_loop == 0):
+		if tempElapsed > 0.1:
+			if run_loop == 0:
 				run_loop = 1
 				set_frame(2)
-			elif(get_frame() == 2):
+			elif get_frame() == 2:
 				set_frame(3)
 				run_loop = 1
-			elif(get_frame() == 6):
+			elif get_frame() == 6:
 				set_frame(5)
 				run_loop = -1
 			#Cycle forwards
-			elif(get_frame() < 6 and run_loop == 1):
+			elif get_frame() < 6 && run_loop == 1:
 				self.set_frame(get_frame() + 1)
 			#Cycle backwards
-			elif(get_frame() > 2 and run_loop == -1):
+			elif get_frame() > 2 && run_loop == -1:
 				self.set_frame(get_frame() - 1)
 			tempElapsed = 0
 
 	#if the player is running left
-	elif( Input.is_action_pressed("run_left")):
-		if(direction == 1):
+	elif  Input.is_action_pressed("run_left"):
+		if direction == 1:
 			direction = -1
 			self.set_flip_h(true)
 		#runElapsed for smoother running
-		if(runElapsed > 0.05):
+		if runElapsed > 0.05:
 			runElapsed = 0
 			pos.x -= speed.x
 			set_pos(pos)
 		self.set_pos(get_pos())
-		if(tempElapsed > 0.1):
-			if(run_loop == 0):
+		if tempElapsed > 0.1:
+			if run_loop == 0:
 				run_loop = 1
 				set_frame(2)
-			elif(get_frame() == 2):
+			elif get_frame() == 2:
 				set_frame(3)
 				run_loop = 1
-			elif(get_frame() == 6):
+			elif get_frame() == 6:
 				set_frame(5)
 				run_loop = -1
 			#Cycle forwards
-			elif(get_frame() < 6 and run_loop == 1):
+			elif get_frame() < 6 && run_loop == 1:
 				self.set_frame(get_frame() + 1)
 			#Cycle backwards
-			elif(get_frame() > 2 and run_loop == -1):
+			elif get_frame() > 2 && run_loop == -1:
 				self.set_frame(get_frame() - 1)
 			tempElapsed = 0
 #If the player shot (button mash, not auto)d
-	if( Input.is_action_pressed("shoot") ):
-		if(tempElapsed > 0.05):
-			if( shot_fired == 0):
+	if  Input.is_action_pressed("shoot"):
+		if tempElapsed > 0.05:
+			if  shot_fired == 0:
 				shot_fired = 1
 				var bullet = preload("res://bullet.scn").instance()
-				bullet.set_pos( get_node("shoot_from").get_global_pos() )
-				if(direction == -1):
+				bullet.set_pos(get_node("shoot_from").get_global_pos())
+				if direction == -1:
 					bullet.speed *= -1
-					bullet.set_pos( Vector2(bullet.get_pos().x - 24, bullet.get_pos().y) )
+					bullet.set_pos(Vector2(bullet.get_pos().x - 24, bullet.get_pos().y))
 				get_node("../..").add_child(bullet)
 				set_frame(1)
 				tempElapsed = 0
-	elif(shot_fired == 1):
+	elif shot_fired == 1:
 		run_loop = 0
 		tempElapsed = 0
 		shot_fired = 0
-		if( get_frame() == 1 ):
+		if get_frame() == 1:
 			set_frame(0)
-		
